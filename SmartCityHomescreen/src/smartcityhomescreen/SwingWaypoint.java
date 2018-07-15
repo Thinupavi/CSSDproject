968589379414c928ade7e820955748516a976d1f
nishanthirad <nishanthirajakumar@gmail.com>
2018-07-15 18:34:04 +0530

    Update to V0.0.6 Alpha
    Added maps

---
+++ SmartCityHomescreen/src/smartcityhomescreen/SwingWaypoint.java
@@ -0,0 +1,71 @@
+package smartcityhomescreen;
+
+import org.jxmapviewer.viewer.DefaultWaypoint;
+import org.jxmapviewer.viewer.GeoPosition;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * A waypoint that is represented by a button on the map.
+ *
+ * @author Daniel Stahr
+ */
+public class SwingWaypoint extends DefaultWaypoint {
+    private final JButton button;
+    private final String text;
+    private final GeoPosition coord;
+
+    public SwingWaypoint(String text, GeoPosition coord) throws IOException {
+        super(coord);
+        this.text = text;
+        this.coord = coord;
+        button = new JButton();
+        //button.setSize(24, 24);
+        //button.setText("Add Sensor Station");
+        //button.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        //button.setPreferredSize(new Dimension(24, 24));
+        Image img = ImageIO.read(getClass().getResource("../marker.png"));
+        button.setIcon(new ImageIcon(img));
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        button.addMouseListener(new SwingWaypointMouseListener());
+        button.setVisible(true);
+        
+    }
+
+    JButton getButton() {
+        return button;
+    }
+    
+
+    private class SwingWaypointMouseListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            JOptionPane.showMessageDialog(button, "You clicked on " + text+ coord.getLatitude());
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+        
+    }
+}
